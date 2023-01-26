6d261519bdf162396e3e9b42c87090577b76cbc0
ridera7 <ridera7@gmail.com>
2023-01-26 23:13:50 +0200

    Added some tests

---
+++ src/main/java/com/epam/Per1/tag/MyTag.java
@@ -0,0 +1,7 @@
+package com.epam.Per1.tag;
+
+import jakarta.servlet.jsp.*;
+import jakarta.servlet.jsp.tagext.SimpleTagSupport;
+
+public class MyTag extends SimpleTagSupport{
+}
--- src/test/java/com/epam/Per1/filter/AuthFilterTest.java
+++ src/test/java/com/epam/Per1/filter/AuthFilterTest.java
@@ -31,28 +31,98 @@ class AuthFilterTest {
     private static Logger log = mock(Logger.class);
 
     @Test
-    public void ifNotAuthorizedThenGoToLoginPage_true() throws IOException, ServletException {
+    public void AuthorizedUser() throws IOException, ServletException {
         UserDTO userDTO = UserDTO.builder()
                 .id(2)
                 .role(new UserRoleDTO(1, "user"))
                 .money(BigDecimal.valueOf(0))
                 .build();
+//        UserDTO userDTO = null;
         User user = Mapper.toUser(userDTO);
-        UserDTO userDTONull = null;
         when(request.getSession()).thenReturn(session);
-        when(session.getAttribute("userDTO")).thenReturn(userDTONull);
+        when(session.getAttribute("userDTO")).thenReturn(userDTO);
         when(userService.getById(2)).thenReturn(user);
         when(request.getServletPath()).thenReturn("/account/");
         when(request.getRequestDispatcher(Pages.LOGIN_PAGE)).thenReturn(requestDispatcher);
         doNothing().when(requestDispatcher).forward(request,response);
 
         AuthFilter authFilter = new AuthFilter(userService, log);
-        System.out.println(request.getSession().getAttribute("userDTO"));
 
         authFilter.doFilter(request,response,chain);
 
+        verify(request, times(4)).getSession();
+        verify(requestDispatcher, times(0)).forward(request, response);
+        verify(chain, times(1)).doFilter(request,response);
 
+    }
 
+    @Test
+    public void NotAuthorizedUser() throws IOException, ServletException {
+        UserDTO userDTO = null;
+        User user = null;
+        when(request.getSession()).thenReturn(session);
+        when(session.getAttribute("userDTO")).thenReturn(userDTO);
+        when(userService.getById(2)).thenReturn(user);
+        when(request.getServletPath()).thenReturn("/account/");
+        when(request.getRequestDispatcher(Pages.LOGIN_PAGE)).thenReturn(requestDispatcher);
+        doNothing().when(requestDispatcher).forward(request,response);
+
+        AuthFilter authFilter = new AuthFilter(userService, log);
+
+        authFilter.doFilter(request,response,chain);
+
+        verify(request, times(2)).getSession();
+        verify(requestDispatcher, times(1)).forward(request, response);
+        verify(chain, times(0)).doFilter(request,response);
+
+    }
+
+    @Test
+    public void AdminAndAccount() throws IOException, ServletException {
+        UserDTO userDTO = UserDTO.builder()
+                .id(2)
+                .role(new UserRoleDTO(1, "administrator"))
+                .money(BigDecimal.valueOf(0))
+                .build();
+        User user = Mapper.toUser(userDTO);
+        when(request.getSession()).thenReturn(session);
+        when(session.getAttribute("userDTO")).thenReturn(userDTO);
+        when(userService.getById(2)).thenReturn(user);
+        when(request.getServletPath()).thenReturn("/account/");
+        when(request.getRequestDispatcher(Pages.LOGIN_PAGE)).thenReturn(requestDispatcher);
+        doNothing().when(requestDispatcher).forward(request,response);
+
+        AuthFilter authFilter = new AuthFilter(userService, log);
+
+        authFilter.doFilter(request,response,chain);
+
+        verify(request, times(5)).getSession();
+        verify(requestDispatcher, times(1)).forward(request, response);
+        verify(chain, times(0)).doFilter(request,response);
+    }
+
+    @Test
+    public void UserAndAdmin() throws IOException, ServletException {
+        UserDTO userDTO = UserDTO.builder()
+                .id(2)
+                .role(new UserRoleDTO(1, "user"))
+                .money(BigDecimal.valueOf(0))
+                .build();
+        User user = Mapper.toUser(userDTO);
+        when(request.getSession()).thenReturn(session);
+        when(session.getAttribute("userDTO")).thenReturn(userDTO);
+        when(userService.getById(2)).thenReturn(user);
+        when(request.getServletPath()).thenReturn("/admin/");
+        when(request.getRequestDispatcher(Pages.LOGIN_PAGE)).thenReturn(requestDispatcher);
+        doNothing().when(requestDispatcher).forward(request,response);
+
+        AuthFilter authFilter = new AuthFilter(userService, log);
+
+        authFilter.doFilter(request,response,chain);
+
+        verify(request, times(5)).getSession();
+        verify(requestDispatcher, times(1)).forward(request, response);
+        verify(chain, times(0)).doFilter(request,response);
     }
 
 }
